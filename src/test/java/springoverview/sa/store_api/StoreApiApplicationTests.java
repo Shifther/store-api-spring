820187c09010f269f5ce8ca9a28cdd3f466ef05e
Carlos Augusto Silva <carlos.silva1@ibm.com>
2024-10-14 15:55:31 -0300

    Adds project initial code

---
+++ src/test/java/springoverview/sa/store_api/StoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package springoverview.sa.store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
